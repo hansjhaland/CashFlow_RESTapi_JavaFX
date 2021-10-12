40666745c05c1ab6200405b8d8910744b4361a25
Hans Jakob Håland <hansjhaa@stud.ntnu.no>
2021-10-12 12:28:58 +0000

    La til serialiserings- og deserialiseringsklasser For Transactions

---
+++ cash-flow/core/src/main/java/json/TransactionDeserializer.java
@@ -0,0 +1,72 @@
+package json;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.DoubleNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+import core.AbstractAccount;
+import core.CheckingAccount;
+import core.SavingsAccount;
+import core.Transaction;
+import core.BSUAccount;
+
+public class TransactionDeserializer extends JsonDeserializer<Transaction>  {
+
+    private AccountDeserializer accountDeserializer = new AccountDeserializer();
+
+    @Override
+    public Transaction deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        TreeNode treeNode = parser.getCodec().readTree(parser);
+        return deserialize((JsonNode) treeNode);
+    }
+
+    public Transaction deserialize(JsonNode jsonNode) {
+        if (jsonNode instanceof ObjectNode) {
+            ObjectNode objectNode = (ObjectNode) jsonNode;
+
+            String payer = "";
+            int payersAccountNumber = 0;
+            String recipient = "";
+            int recipientsAccountNumber = 0;
+            double amount = 0.0;
+
+            JsonNode payerNode = objectNode.get("payer");
+            if (payerNode instanceof TextNode){
+                payer = payerNode.asText();
+            }
+
+            JsonNode payersAccountNumberNode = objectNode.get("payersAccountNumberNode");
+            if (payersAccountNumberNode instanceof IntNode){
+                payersAccountNumber = payersAccountNumberNode.asInt();
+            }
+
+            JsonNode recipientNode = objectNode.get("recipient");
+            if (recipientNode instanceof TextNode){
+                recipient = recipientNode.asText();
+            }
+
+            JsonNode recipientsAccountNumberNode = objectNode.get("recipientsAccountNumberNode");
+            if (recipientsAccountNumberNode instanceof IntNode){
+                recipientsAccountNumber = recipientsAccountNumberNode.asInt();
+            }
+
+            JsonNode amountNode = objectNode.get("amount");
+            if (amountNode instanceof DoubleNode){
+                amount = amountNode.asDouble();
+            }
+
+            return new Transaction(payer, recipient, payersAccountNumber, recipientsAccountNumber, amount);
+        }
+        return null;
+    }
+    
+}
