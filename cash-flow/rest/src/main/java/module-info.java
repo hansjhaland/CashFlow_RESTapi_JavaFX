aa057d4600bf57c0790ea4be5588e4677c892d4e
Arild Strømsvåg <aris@stud.ntnu.no>
2021-11-04 14:31:23 +0100

    La til REST-modul La til mvn bygg for rest-api og rest-server med tilhørende avhengigheter

---
+++ cash-flow/rest/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module casflow.rest {
+    requires jakarta.ws.rs;
+
+    requires jersey.common;
+    requires jersey.server;
+    requires jersey.media.json.jackson;
+
+    requires org.glassfish.hk2.api;
+    requires org.slf4j;
+
+    requires cashflow.core;
+
+    opens restapi to jersey.server;
+}
