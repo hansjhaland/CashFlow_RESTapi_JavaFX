022ddc3755f48fe8d197c31c0e3f10e3e83dcc04
Hans Jakob Håland <hansjhaa@stud.ntnu.no>
2021-11-09 17:00:43 +0100

    Oppdaterte config og objectmapper i restserver

--- cash-flow/rest/src/main/java/restserver/CashFlowConfig.java
+++ cash-flow/rest/src/main/java/restserver/CashFlowConfig.java
@@ -1,3 +1,43 @@
-public class CashFlowConfig {
+package restserver;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import core.User;
+import json.CashFlowPersistence;
+import core.AbstractAccount;
+import restapi.UserRestService;
+public class CashFlowConfig extends ResourceConfig{
     
+    private User user;
+    private CashFlowPersistence cfp;
+
+    public CashFlowConfig(User user) {
+        setUser(user);
+        cfp = new CashFlowPersistence();
+        register(UserRestService.class);
+        register(UserObjectMapperProvider.class);
+        register(JacksonFeature.class);
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(CashFlowConfig.this.user);
+                bind(CashFlowConfig.this.cfp);
+            }
+        });
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    private void setUser(User user) {
+        this.user = user;
+    }
+
 }
--- cash-flow/rest/src/main/java/restserver/UserObjectMapperProvider.java
+++ cash-flow/rest/src/main/java/restserver/UserObjectMapperProvider.java
@@ -1,3 +1,27 @@
-public class UserObjectMapperProvider {
+package restserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.ext.ContextResolver;
+import jakarta.ws.rs.ext.Provider;
+import json.CashFlowPersistence;
+
+@Provider
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserObjectMapperProvider implements ContextResolver<ObjectMapper> {
     
+    private final ObjectMapper objectMapper;
+
+    public UserObjectMapperProvider(){
+        objectMapper = CashFlowPersistence.createObjectMapper();
+    }
+
+    @Override
+    public ObjectMapper getContext(final Class<?> type){
+        return objectMapper;
+    }
+
 }
