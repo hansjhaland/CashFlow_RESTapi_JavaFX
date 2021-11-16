e69522a850ae3e41a4fadc8cee77bea18519385c
Lise Jakobsen <lisejak@stud.ntnu.no>
2021-11-16 13:18:11 +0000

    Satte opp strukturen for integrasjonstest, og ferdig med klassediagram (for nå)

---
+++ cash-flow/integrationtests/src/test/java/CashFlowAppIT.java
@@ -0,0 +1,56 @@
+package java;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class CashFlowAppIT extends ApplicationTest {
+
+  @BeforeAll
+  public static void setupHeadless() {
+    CashFlowApp.supportHeadless();
+  }
+
+  private CashFlowController controller;
+
+  @Override
+  public void start(final Stage stage) throws Exception {
+    final FXMLLoader loader = new FXMLLoader(getClass().getResource("CashFlowApp_it.fxml"));
+    final Parent root = loader.load();
+    this.controller = loader.getController();
+    stage.setScene(new Scene(root));
+    stage.show();
+  }
+
+  @BeforeEach
+  public void setupItems() throws URISyntaxException {
+    // same as in test-todolist.json (should perhaps read it instead)
+    try (Reader reader = new InputStreamReader(getClass().getResourceAsStream("it-cashflow.json"))) {
+      String port = System.getProperty("todo.port"); //hvor skal dette?
+      assertNotNull(port, "No todo.port system property set");
+      URI baseUri = new URI("http://localhost:" + port + "/cashflow/"); //??
+      System.out.println("Base RemoteTodoModelAcces URI: " + baseUri); //??
+      this.controller.setTodoModelAccess(new RemoteTodoModelAccess(baseUri));
+    } catch (IOException ioe) {
+      fail(ioe.getMessage());
+    }
+  }
+
+  @Test
+  public void testController_initial() {
+    assertNotNull(this.controller);
+  }
+
+}
