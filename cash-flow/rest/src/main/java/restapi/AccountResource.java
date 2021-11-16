c30d6555298f34498d151a013c4531a850e98bb2
Arild Strømsvåg <aris@stud.ntnu.no>
2021-11-16 17:13:28 +0100

    Endret på logging og lagt til Java-doc

--- cash-flow/rest/src/main/java/restapi/AccountResource.java
+++ cash-flow/rest/src/main/java/restapi/AccountResource.java
@@ -14,6 +14,10 @@ import org.slf4j.LoggerFactory;
 import core.User;
 import core.AbstractAccount;
 
+/**
+ * Sub-resource of UserRestService. Is used to update information regarding
+ * the users accounts.
+ */
 @Produces(MediaType.APPLICATION_JSON)
 public class AccountResource {
     
@@ -26,38 +30,64 @@ public class AccountResource {
     @Context
     private CashFlowPersistence cfp;
 
+    /**
+     * Initializes an AccountResource-object which represents an account.
+     * @param user the owner of the account
+     * @param accountNumber the accountnumber
+     * @param account the account-object
+     */
     public AccountResource(User user, String accountNumber, AbstractAccount account){
         this.user = user;
         this.accountNumber = accountNumber;
         this.account = account;
     }
 
+    /**
+     * Checks if the current user-object has the spesified account.
+     */
     private void checkIfAccountExists(){
         if (account == null){
             throw new IllegalStateException("The account " + account + " does not exist");
         }
     }
 
+    /**
+     * Method for getting the account, indicating
+     * that the method responds to HTTP-GET requests.
+     * @return the account
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public AbstractAccount getAccount() {
         checkIfAccountExists();
-        LOG.debug("getAccount()", accountNumber);
+        LOG.debug("getAccount(): " + account.getName() + " (" + accountNumber + ")");
         return this.account;
     }
 
-    //?????
+    /**
+     * Method for adding an account or updating the account if 
+     * it already exists, indicating that the method responds 
+     * to HTTP-PUT requests.
+     * @param account the account to be put
+     * @return {@code true} if the account was put
+     */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public boolean putAccount(AbstractAccount account){
-        LOG.debug("putAccount(): ", account.getAccountNumber());
+        LOG.debug("putAccount(): " + account.getName() + " (" + accountNumber + ")");
         return user.addAccount(account);
     } 
 
+    /**
+     * Method for deleting/removing an account, indicating
+     * that the method responds to HTTP-DELETE requests.
+     * @return {@code true} if the account was deleted
+     */
     @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
     public boolean removeAccount(){
         checkIfAccountExists();
-        this.user.removeAccount(this.account);
-        return true; 
+        LOG.debug("deleteAccount(): " + account.getName() + " (" + accountNumber + ")");
+        return user.removeAccount(this.account);
     }
 }
--- cash-flow/rest/src/main/java/restserver/CashFlowConfig.java
+++ cash-flow/rest/src/main/java/restserver/CashFlowConfig.java
@@ -1,28 +1,28 @@
 package restserver;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import core.User;
-import json.CashFlowPersistence;
-import core.AbstractAccount;
 import core.CheckingAccount;
 import core.SavingsAccount;
 import restapi.UserRestService;
 
+/**
+ * Configures the REST-service, saying it should use
+ * JSON with Jackson library, inject a User-object
+ * using a binder, etc.
+ */
 public class CashFlowConfig extends ResourceConfig{
     
     private User user;
-    //private CashFlowPersistence cfp;
 
+    /**
+     * Initializes the CashFlowConfig with the given user.
+     * @param user the user that is going to get injected
+     */
     public CashFlowConfig(User user) {
         setUser(user);
-        //cfp = new CashFlowPersistence();
         register(UserRestService.class);
         register(UserObjectMapperProvider.class);
         register(JacksonFeature.class);
@@ -30,11 +30,14 @@ public class CashFlowConfig extends ResourceConfig{
             @Override
             protected void configure() {
                 bind(CashFlowConfig.this.user);
-               // bind(CashFlowConfig.this.cfp);
             }
         });
     }
 
+    /**
+     * Initializes the CashFlowConfig with a default User-object
+     * each time the server is started.
+     */
     public CashFlowConfig() {
         this(createDefaultUser());
     }
@@ -47,6 +50,10 @@ public class CashFlowConfig extends ResourceConfig{
         this.user = user;
     }
 
+    /**
+     * Creates a default User-object.
+     * @return the default user
+     */
     private static User createDefaultUser() {
         User defaultUser = new User(654321);
         new CheckingAccount("Checking", 100, 2345, defaultUser);
