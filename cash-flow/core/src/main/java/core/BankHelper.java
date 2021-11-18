0a930a2e8efa0fe9e8ea5384bffa0708c06ab6ff
Arild Strømsvåg <aris@stud.ntnu.no>
2021-11-18 14:55:09 +0100

    Lagt til litt java-doc Lagt til java-doc i BankHelper og AbstractAccount.

--- cash-flow/core/src/main/java/core/BankHelper.java
+++ cash-flow/core/src/main/java/core/BankHelper.java
@@ -19,6 +19,12 @@ public final class BankHelper {
         }
     }
 
+    /**
+     * Checks if the account number is taken.
+     * @param accountNumber the account number to check
+     * @param user the user to check
+     * @throws IllegalStateException if the account number is taken
+     */
     static void checkIfAccountNumberIsTaken(int accountNumber, User user) {
         for (int exisitingAccountNumber : user.getAccountNumbers()) {
             if (exisitingAccountNumber == accountNumber) {
@@ -51,6 +57,11 @@ public final class BankHelper {
     }
 
 
+    /**
+     * Checks if the account number is between 1000 and 9999.
+     * @param accountNumber the acocunt number to be checked
+     * @throws IllegalArgumentException if the account number is not between 1000 and 9999
+     */
     static void checkIfValidAccountNumber(int accountNumber) {
         if (accountNumber < 1000 || accountNumber > 9999) {
             throw new IllegalArgumentException("Accountnumber must be between 1000 and 9999, but was: " + accountNumber);
@@ -73,7 +84,7 @@ public final class BankHelper {
      * @param amount the amount to check
      * @return {@code true} if the amount is positive
      */
-    static public boolean isPositiveAmount(double amount) {
+    public static boolean isPositiveAmount(double amount) {
         return amount >= 0;
     }
 
@@ -83,12 +94,17 @@ public final class BankHelper {
      * @param amount the amount to be added
      * @return {@code false} if the balance is set to be negative
      */
-    static public boolean isBalanceValidWhenAdding(double amount, AbstractAccount account) {
+    public static boolean isBalanceValidWhenAdding(double amount, AbstractAccount account) {
         double newBalance = account.getBalance() + amount;
         return newBalance >= 0;
     }
 
-    static public boolean hasBSU(User user) {
+    /**
+     * Checks if the user already owns a BSU-account.
+     * @param user the user to be checked
+     * @return {@code true} if the user owns a BSU-account
+     */
+    public static boolean hasBSU(User user) {
         return user.getAccounts().stream().anyMatch(account -> account instanceof BSUAccount);
     }
 
@@ -97,14 +113,19 @@ public final class BankHelper {
      * @param name the name to be checked
      * @return {@code true} if the name satisfies this rule
      */
-    static public boolean isValidName(String name) {
+    public static boolean isValidName(String name) {
         if (name.length() > 20 || !isOnlyLettersAndSpaces(name)) {
             return false;
         }
         return true;
     }
 
-    static public boolean isOnlyLettersAndSpaces(String s) {
+    /**
+     * Checks if the given string consists of only letters and spaces.
+     * @param s the string to check
+     * @return {@code true} if name consists of only letters and spaces
+     */
+    public static boolean isOnlyLettersAndSpaces(String s) {
         for(int i = 0; i < s.length(); i++){
           char ch = s.charAt(i);
           if (Character.isLetter(ch) || ch == ' ') {
